11f72727ff3ebad5a0441b727317c3345112718b
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-05-17 22:17:13 -0300

    Update my_graph.py

--- my_graph.py
+++ my_graph.py
@@ -12,3 +12,10 @@ class Graph:
             return True
         return False
     
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    
